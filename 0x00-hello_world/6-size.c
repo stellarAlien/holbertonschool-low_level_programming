6ef29906489e5633d225d175799a7ade82025927
Seif <saifgaida@gmail.com>
2021-09-30 08:32:32 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,6 @@ long long int lli;
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+return (0);
 }
 
