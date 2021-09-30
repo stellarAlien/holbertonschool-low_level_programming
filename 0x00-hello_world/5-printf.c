c93f30671cd718da20db6ddddef999d8b64aa63b
Seif <saifgaida@gmail.com>
2021-09-30 06:03:46 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
-/*with proper grammar, but the outcome is a piece of art,
- *
- * /
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		printf("Holberton School");
+			return (0);
+}
 int main(void)
 {
 	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
