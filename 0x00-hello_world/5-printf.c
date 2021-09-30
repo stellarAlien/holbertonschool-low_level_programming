afbeec95dceed8841cee17dc92195a1ca77e71c4
Seif <saifgaida@gmail.com>
2021-09-30 05:57:59 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,6 @@
 /*with proper grammar, but the outcome is a piece of art,*/
 int main(void)
 {
-	printf("%s","with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
