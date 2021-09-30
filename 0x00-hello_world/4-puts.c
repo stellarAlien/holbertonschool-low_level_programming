5c37e49b181a8d1b70af4ad570fe61afb3624f49
Seif <saifgaida@gmail.com>
2021-09-30 05:35:05 -0700

    msg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
+
+/**
+ *  *main - funtion display "Programming is like building a multilingual puzzle.
+ *   *Return: 0
+ *    */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
