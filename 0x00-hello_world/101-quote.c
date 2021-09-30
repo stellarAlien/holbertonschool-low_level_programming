ea1e3025ff1dd619f34d58a71c61409019a0b4cf
Seif <saifgaida@gmail.com>
2021-09-30 12:30:02 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
+
+#include <unistd.h>
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
+ *  *main - display and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *   *Return: 1
  */
+
 int main(void)
 {
-fprintf stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-return (1);
+		write(2, "and that piece of art is useful\" \
+				- Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
