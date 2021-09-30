2a45501bb1bcfd74c3032398ebe6f7d39ae34884
Seif <saifgaida@gmail.com>
2021-09-30 11:43:42 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,12 @@ float x;
 char c;
 long int li;
 long long int lli;
+
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int:	%zu byte(s)\n",	sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float);
-
 return (0);
 }
 
