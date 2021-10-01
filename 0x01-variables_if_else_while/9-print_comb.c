bd59fa0588bcb90b56316f307a086af5dbf99343
Seif <saifgaida@gmail.com>
2021-10-01 11:20:02 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <time.h>
+#include<stdlib.h>
+#include<stdio.h>
+/**
+*      * main - Entry point
+*           *
+*                * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = '0'; i <= '8'; i++)
+{
+putchar('i');
+putchar(',');
+putchar(' ');
+}
+putchar ('9');
+return (0);
+}
