d339ef54458aa5506069a2d42572511052de881c
Seif <saifgaida@gmail.com>
2021-10-01 09:35:17 -0700

    Aa

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <time.h>
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	 char i;
+for (i = 'a'; i < 'z'; i++)
+{
+	putchar(i);
+}
+for (i = 'A'; i <= 'Z'; i++)
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);
+}
