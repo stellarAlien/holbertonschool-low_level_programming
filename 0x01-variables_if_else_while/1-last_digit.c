73212d1af1770daf112663759db35daaa056201d
Seif <saifgaida@gmail.com>
2021-10-01 08:02:33 -0700

    cmt

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ int main(void)
 					printf("Last digit of %d is ", n);
 					if (((n % 10) < 6) && (n % 10) != 0)
 					{
-						printf(" %d and is less than 6 and not 0", n % 10);
+						printf(" %d and is less than 6 and not 0\n", n % 10);
 					}
 					if ((n % 10) == 0)
 					{
-						printf("0 and is 0");
+						printf("0 and is 0\n");
 					}
 					else if ((n % 10) > 5)
 					{
-						printf("%d and is greater than 5", n % 10);
+						printf("%d and is greater than 5\n", n % 10);
 					}
 					return (0);
 }
