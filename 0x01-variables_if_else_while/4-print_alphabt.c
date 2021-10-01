1867014d64c9df62a50aea9bd422dd78ed45b042
Seif <saifgaida@gmail.com>
2021-10-01 09:53:48 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include<time.h>
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char i = 'a';
+do {
+if (i == 'e' || i == 'q')
+{
+i++;
+}
+else
+{
+putchar(i);
+i++;
+} while (i < = 'z');
+putchar('\n');
+return (0);
+}
