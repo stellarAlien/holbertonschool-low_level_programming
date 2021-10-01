a822e04b20ef7b27d35064cd370e6d8944fc09f8
Seif <saifgaida@gmail.com>
2021-10-01 08:26:46 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <time.h>
+#include<stdlib.h>
+#include<stdio.h>
+/*
+ * *main prints alphabet
+ * *return: 0
+ */
+int main(void)
+{
+for (int i = 'a'; i < 'z'; i++)
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);
+}
