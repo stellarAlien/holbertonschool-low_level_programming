f46f9b5a7ccff1b79c852ef2adac83ee72216770
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-04 20:08:57 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,27 +6,30 @@
 int main(void)
 {
 int i, j , k , v;
-for(i = '0'; i <='7' ; i++){
-for(j = i + 1;j <= '8'; j++)
+for(i = '0' ; i <= '7'  ; i++)
+{
+for(j = i + 1; j <= '8'; j++)
 {
 for(k = '2';k <= '9'; k++)
+ {
+if(i == '7' && j == '8' && k == '9')
 {
-if(i == '7' && j == '8' && k == '9'){
-v = 1; 
+v = 1;
 }
-if(i < j && j < k){
+if(i < j && j < k)
+{
 putchar(i);
 putchar(j);
 putchar(k);
 if(v == 1)
 {
 break;
-}               
+}
 putchar(',');
-putchar(' ');   
-}            
-}              
-}          
+putchar(' ');
+}
+}
+}
 }
 putchar('\n');
 return(0);
