717435cc045fe0932c9e2ca7ba00583f1c1fece8
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-04 20:02:23 +0100

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+int i,j,k,v;
+for(i='0';i<='7';i++){
+for (j=i+1;j<='8';j++)
+{
+for(k='2';k<='9';k++)
+{
+if(i == '7' && j == '8' && k == '9'){
+v = 1; 
+}
+if(i<j && j<k){
+putchar(i);
+putchar(j);
+putchar(k);
+if(v == 1)
+{
+break;
+}               
+putchar(',');
+putchar(' ');   
+}            
+}              
+}          
+}
+putchar('\n');
+return(0);
+}
