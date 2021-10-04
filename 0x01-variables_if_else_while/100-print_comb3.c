6c8eaffd34d2e20bd1f3157f397ea095badedd57
Seif <saifgaida@gmail.com>
2021-10-04 01:15:07 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
-int main()
+/**
+ *  *  *  * main - Entry point
+ *   *   *   *
+ *    *    *    * Return: Always 0 (Success)
+ */
+int main(void)
 {
-int i,j;
-for(i = '0'; i <= '8'; i++)
+int i, j;
+for (i = '0'; i <= '8'; i++)
 {
-for (j = i+1; j <= '9'; j++)
+for (j = i + 1; j <= '9'; j++)
 {
-if(i < j){
+if (i < j)
+{
 putchar(i);
 putchar(j);
-if(i == '8' && j == '9')
+if (i == '8' && j == '9')
 {
-break;														
-}													                putchar(',');
+break;
+}
+putchar(',');
 putchar(' ');
 }
 }
 }
-
+}
