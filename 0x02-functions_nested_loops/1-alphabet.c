bd6e68c8fbaf2a5c50dec36aa4d669896686d1ec
Seif <saifgaida@gmail.com>
2021-10-05 02:49:58 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *  * print_alphabet - check the code
+ *   *
+ *    * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar ('i');
+}
+putchar('\n');
+}
+/**
+ *  * main - Entry point
+ *   * Return: 0
+ */
+int main(void)
+{
+print_alphabet();
+}
