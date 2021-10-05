43a9f7e08a3fbb8f54fe1f76f5e553a3272b59a7
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-05 18:46:48 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,16 +7,15 @@
  int print_last_digit(int n)
 {
 int x;
-x = n%10;
+x = n % 10;
 if (x < 0)
 {
 x = x * -1;
-_putchar('-');
-_putchar(x+'0');
+_putchar(x + '0');
 }
 else
 {
-_putchar(x+'0');
+_putchar(x + '0');
 }
 return (x);
 }
