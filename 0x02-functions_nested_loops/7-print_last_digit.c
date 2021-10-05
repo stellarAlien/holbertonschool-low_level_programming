e70d32dcbf3ce5d9157ed70cd04edc7710d9a448
Seif <saifgaida@gmail.com>
2021-10-05 08:53:54 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,15 +3,18 @@
  *  *print_last_digit - display last digit
  *   *@n: parameter integer
  *    *Return: last digit
- *     */
+ */
 int print_last_digit(int n)
 {
-if( n < 0){
-_putchar((n % 10) * -1);
+x = n % 10
+if (x < 0)
+{
+x = x * -1;
+_putchar(x % 10);
 }
 else
 {
-_putchar(n % 10);
+_putchar(x % 10);
 }
 return (1);
 }
