bd7d4008cde7ad3aa2aa3f842542ce84c19403b6
Seif <saifgaida@gmail.com>
2021-10-05 08:46:53 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *  *print_last_digit - display last digit
+ *   *@n: parameter integer
+ *    *Return: last digit
+ *     */
+int print_last_digit(int n)
+{
+_putchar(n % 10);
+return (1);
+}
+
