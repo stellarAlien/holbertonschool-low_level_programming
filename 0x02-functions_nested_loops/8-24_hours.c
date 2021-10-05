b6618d94fe4dcb1a0557ab487e363bef16825503
Seif <saifgaida@gmail.com>
2021-10-05 14:59:31 -0700

    24hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ *  *jack_bauer - display 24hours
+ *   *Return: void
+ */
+void jack_bauer(void)
+{
+int i, j, k, l;
+for (i = '0'; i <= '2'; i++)
+{
+if (v)
+{
+break;
+}
+for (j = '0'; j <= '9'; j++)
+{
+for (k = '0'; k <= '5'; k++)
+{
+for (l = '0'; l <= '9'; l++)
+{
+if !(i == '2' && j == '3' && k == "5" && l == "9")
+{
+_putchar(i);
+_putchar(j);
+_putchar(':');
+_putchar(k);
+_putchar(l);
+}
+else
+{
+v = 1;
+break;
+}
+}
+}
+}
+}
+}
