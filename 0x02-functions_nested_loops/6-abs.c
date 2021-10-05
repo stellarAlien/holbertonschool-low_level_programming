bee65c99aefed94badb055ddd01be3d6dcd3b293
Seif <saifgaida@gmail.com>
2021-10-05 08:35:34 -0700

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,12 +3,12 @@
  *  *_abs - display absolute number
  *   *@n: parameter integer
  *    *Return: absolute number
- *     */
+ */
 int _abs(int n)
 {
 if (n < 0)
 {
-n=n*-1;
+n = n * -1;
 }
 return (n);
 }
