b8433fe426e90c41708d57d5ac27b73176c9a631
Seif <saifgaida@gmail.com>
2021-10-05 03:00:22 -0700

    print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,29 @@
 #include<stdio.h>
+/**
+ *  *  * print_alphabet - check the code
+ *   *   *
+ *    *    * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar ('i');
+}
+_putchar('\n');
+}
+/**
+ *  *  * main - Entry point
+ *   *   * Return: 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+print_alphabet();
+}
+}
 
