d49af574c718dd46ec95408aa827794c5e5090c7
Seif <saifgaida@gmail.com>
2021-10-05 08:07:59 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  *  *_print_sign - display 1 is lower char, 0 otherwise
+ *   *   *@c: The character or ascii code
+ *    *    *Return: 0 or 1
+ */
+int print_sign(int n)
+{
+if (n < 0)
+{
+_puthcar('-');
+return (-1);
+}
+else if (n > 0)
+{
+_puthcar('+');
+return (1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
