92febc4535a3030a6eac336ac9ed946e05aacd12
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-05 13:45:13 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,15 @@
 void print_alphabet_x10(void)
 {
 int i;
-char c ;
+char c;
 for (i = 0; i <= 9; i++)
-{ for (c = 'a'; c <= 'z'; c++)
+{ 
+  for (c = 'a'; c <= 'z'; c++)
 {
-_putchar(c); 
-}
+_putchar(c);
 }
 _putchar('\n');
+}
 }
 
 
