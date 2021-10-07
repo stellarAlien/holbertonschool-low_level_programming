d64fa4fb3cbc24e99baf463bb0b3616b8f46b2b5
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-07 23:01:06 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,21 @@
 #include "main.h"
- /**
- *print_most_numbers : see function
- * return : 0
+
+/**
+ * print_most_numbers - print numbers
+ *
+ * Return: 1 or 0
  */
- void print_most_numbers(void)
- {
- int i;
- for (i = 0; i < 10; i++)
- {
- if !(i == 2 || i == 4)
- {
- _putchar(i +'0');
- }
- else
- continue;
- _putchar('\n');
- }
+
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+if ((i == 50) || (i == 52))
+continue;
+else
+_putchar(i);
+}
+_putchar('\n');
+}
