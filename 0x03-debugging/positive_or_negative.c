e23ebb0bfdb4762e7e536cbe40a8ca8198b0220f
Seif <saifgaida@gmail.com>
2021-10-07 02:43:16 -0700

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+int main(void)
+{
+if (i < 0)
+{
+printf("%d is negative\n", n);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+printf("%d is positive\n", i);
+}
+return (0);
+}
