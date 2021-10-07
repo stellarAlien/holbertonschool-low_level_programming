67a53969a805a1fd1e8998ef8cff82849c3db92c
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-07 22:58:49 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * print_numbers - print numbers
- *
- * Return: 1 or 0
- */
+#include <stdio.h>
 
-void print_numbers(void)
-{
-int i;
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
-for (i = 48; i < 58; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
-}
+#endif /* MAIN_H */
