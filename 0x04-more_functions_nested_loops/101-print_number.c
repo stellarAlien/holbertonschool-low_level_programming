0c4e262cab72c91e58584cd406a34d70d5890000
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-07 23:36:49 +0100

    Create 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/* print_number - see function
+* return : always 0
+*/
+void print_number(int n)
+{
+int x;
+while(n > 10)
+{
+    x=n % 10;
+    int n = (int)n / 10;
+    printf('%d', x);
+}
+printf('%d', n);
+}
