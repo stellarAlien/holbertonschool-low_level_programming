2b0fadc2cd777bab61947c71d48ce4e81999df88
Seif <saifgaida@gmail.com>
2021-10-07 01:12:37 -0700

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -9,13 +9,7 @@ int main(void)
 {
 	        int i;
 		i = 0;
-			 if ( positive_or_negative(i) == 0)
-			 {
-				printf("0 is zero");
-				 return(1);
-			 }
-			 else
-			 {
-		 		 return (0);
-}
+			positive_or_negative(i);
+
+			        return (0);
 }
