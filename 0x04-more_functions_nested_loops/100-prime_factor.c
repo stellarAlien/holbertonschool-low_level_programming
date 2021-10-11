59427b4c001acd48b1250e2cdaec150a4534f305
Seif <saifgaida@gmail.com>
2021-10-11 08:22:51 -0700

    4

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,20 +12,20 @@ boolean prime( int n)
 {
 boolean v;
 int i;
-for(i = 2; i <= n/2; i++){
+for(i = 2; i <=  n / 2; i++){
 if((n % i) == 0)
 {
 v = 1;
 break;
 }
-}  
+}
 return (v);
-} 
+}
 int main (void)
 {
 long int i, n, m;
 n = 612852475143;
-for (i = 1; i <= n/2 ; i++)
+for (i = 1; i <= n / 2 ; i++)
 {
 if(!prime(i))
 {
