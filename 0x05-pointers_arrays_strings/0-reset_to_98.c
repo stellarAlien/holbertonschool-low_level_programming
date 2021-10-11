f2afcc69695c28e1c89398100a3071505a383991
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-11 13:13:16 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
+*function to update a value
 * reset_to_98 : read function
-* @*n : int to update
+* @n : int to update
 */
 void reset_to_98(int *n)
 {
-*n=98;
+*n = 98;
 }
