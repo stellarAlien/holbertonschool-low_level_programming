3772a92ce392eb324fb7aaa360d3322b66d1bb4b
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-11 22:41:52 +0100

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,14 +6,15 @@
  *    *Return: void
  */
 void rev_string(char *s)
-{	
+{
 int n, i, x;
-n = strlen(s);
+i = 0;
+n = strlen (s);
 while( i <=  n / 2)
 {
 x=s[i];
-s[i] = s[n - i];
-s[n - i] = x;
+s[i] = s[n - i - 1];
+s[n - i] 	= x;
 i++;
 }
 }
