ad1ce07745cfa87199c7ef114a7284d4778abad6
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-11 22:49:25 +0100

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ *  *puts2 - Write a function that prints every other
+ *   *character of a string, starting with the first
+ *    *character, followed by a new line.
+ *     *@str: pointer char
+ *      *Return: void
+ */
 void puts2(char *str)
 {
 int i;
