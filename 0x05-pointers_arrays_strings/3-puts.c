0b9c4bb4f2db3eb6bc4786d8946b7ec92eab108d
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-11 22:22:13 +0100

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,10 @@
 #include "main.h"
+
+/**
+ *  *_strlen - return size of array char
+ *   *@s: pointer integer
+ *    *Return: size of array char
+ */
 void _puts(char *str)
 {
 fprintf(stdout, "%s\n", str);
