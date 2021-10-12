630896d41aa6de078085ea1313574012272cd4be
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-12 16:41:57 +0100

    100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include<string.h>
+int _atoi(char *s)
+{
+/*
+ *@a
+*/ 
+int i,a;
+switch (s[i])
+{
+	 case('\0');
+	  {
+		   break;
+		    }
+	   case ('-'):
+	   a = 0;
+	    i++;
+	     case(+):
+	     a = 1;
+	      i++;
+	       case ( s[i] <='0' && s[i] == '9'):
+	       if(a)
+	       {
+		       printf("%d", s[i]-48);
+		       i++;
+	       }
+	       else 
+	       {
+printf("-%d", s[i]-48);
+}
+default :
+{
+i++;
+continue ;
+}
+}
+
