5bc62440524e98992101b4c41e4ba263fdd97c3f
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-14 22:37:02 +0100

    fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,3 @@
-
 /**
  * _strncpy- link two strings
  * @dest :string to be concatenated with src
@@ -8,7 +7,7 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0;
+int i;
 for (i = 0; src[i] != '\0' && i < n; i++)
 {
 dest[i] = src[i];
