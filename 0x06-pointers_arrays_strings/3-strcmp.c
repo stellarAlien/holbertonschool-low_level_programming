cf29e9a68a981de38265ebecd898255d4a2853de
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-14 22:42:42 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,37 +1,23 @@
+#include "main.h"
 
-/* _strcmp _ comapres two strings 
-* @s1 :first string
-* @s2 :second string
-*/
+/**
+ * _strcmp - entry point
+ * @s1:char
+ * @s2:char
+ * Return:char
+ */
 int _strcmp(char *s1, char *s2)
 {
-int i, v;
-while(s1[i] && s2[i] )
+while ((*s1 != '\0') && (*s2 != '\0'))
 {
-if(s1[i] < s2[i])
+if (*s1 == *s2)
 {
-v = -1;
-break;    
-}
-else if (s1[i] > s2[i] ) 
-{
-v =1;
-break;
-}
-else
+s1++;
+s2++;
 continue;
 }
-if(s1[i] == '\0' && !(v))
-{
-v = -1;
+else if (*s1 != *s2)
+return (*s1 - *s2);
 }
-else if ( s2[i] == '\0' && !(v))
-{
-v = 1;    
-} 
-else 
-{
-v = 0;
-}
-return(v);
+return (0);
 }
