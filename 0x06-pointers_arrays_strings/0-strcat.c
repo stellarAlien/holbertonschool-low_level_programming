35be19d05fc2856f8329a96f43b605729f4a63e9
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-14 11:42:46 +0100

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,19 @@
 #include "main.h"
+#include<string.h>
+/**
+ * _strcat - concatenates
+ * @dest : dest
+ * @src : src
+ */
 char *_strcat(char *dest, char *src)
+
 {
-return(*dest + *src);
+int n;
+n = strlen(src);
+l = strlen(dest);
+for (i = l; dest[i] &&  i < n ; i++ )
+{
+dest[i] = src[j++];
+}
+dest[i] = '\0';
 }
