1e647da76394660a85da2f9effb44b95dfab7d36
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-14 23:35:32 +0100

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,24 @@ void reverse_array(int *a, int n)
 {
 int x,j,i;
 j = n-1;
-while (i != j)
+if( (j%2) != 0 )
+while (i += 1 != j)
 {
 x = a[i];
 a[i] = a[j];
 a[j] = x;
 j--;
 i++;
+}
+else{
+while( i!= j)
+{
+x = a[i];
+a[i] = a[j];
+a[j] = x;
+j--;
+i++;
+}
+}
 }
 }
