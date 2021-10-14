cb92a22882e45ead900136862533d445b39286bf
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-14 23:10:40 +0100

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/*
+* reverse_array : reversa an array
+* @a :array
+* @n :array size
+*/
+void reverse_array(int *a, int n)
+{
+int x,i;
+if( (n%2) == 0)
+{
+    n = n/2;
+}
+else
+n = ((n-1)/2);
+for (i = 0 ; i < n/2; i++)
+{
+x = a[n - i -1];
+a[n -i -1] = a[i];
+a[i] = x;
+}
+}
