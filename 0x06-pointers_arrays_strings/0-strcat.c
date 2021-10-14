b3e7fa1be4141eea5bd5b9709976fffb16e2279c
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-14 20:45:21 +0100

    0 nw

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,26 @@
-#include "main.h"
 /**
- * _strcat)- link two strings
- * @dest :string to be concatenated with src
- * @src : string to concatenate to dest
- * Return: dest
+ *_strcat - concatenates two strings.
+ *@dest: pointer parameter destination
+ *@src: pointer parameter source
+ *Return: dest variable
  */
 char *_strcat(char *dest, char *src)
 {
-int l = 0, i;
+	int i = 0, j = 0, y = 0, x = 0;
 
-while (dest[l])
-{
-l++;
-}
+	while (src[i])
+		i++;
 
-for (i = 0; src[i] != '\0'; i++)
-{
-dest[l] = src[i];
-l++;
-}
+	while (dest[j])
+		j++;
 
-dest[l] = '\0';
-return (dest);
+	i += j;
+
+	for (y = j; y < i; y++)
+	{
+		dest[y] = src[x];
+		x++;
+	}
+
+	return (dest);
 }
