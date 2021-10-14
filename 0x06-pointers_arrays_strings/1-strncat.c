b6bae26b29216c4f2ea6a64c67ade6077b959ccc
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-14 11:12:48 +0100

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<string.h>
+/**
+* 
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int d;
+d = strlen(dest)
+for (i = d; i <= n; i++)
+{
+dest[i] = src [i];    
+}    
+src[i] = '\0';
+}
