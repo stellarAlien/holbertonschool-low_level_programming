508200e4254334b5af82bbf940fc0fe05dded856
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-18 23:34:46 +0100

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,4 @@
-    #include "main.h"
-
+#include "main.h"
 /**
  *print_chessboard - prints the chessboard.
  *@a: pointer parameter matrix
@@ -7,16 +6,15 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
-	char b;
-
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			b = a[i][j];
-			_putchar(b);
-		}
-		_putchar('\n');
-	}
+int i, j;
+char b;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+b = a[i][j];
+putchar(b);
+}
+putchar('\n');
+}
 }
