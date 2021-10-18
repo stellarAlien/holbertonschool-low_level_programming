96f6cdbc524f85847d6b543b5f6166b23d30ff58
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-18 22:32:10 +0100

    ??

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,17 @@
 #include "main.h"
 /**
- * _strncat - link two strings
- * @dest :string to be concatenated with src
- * @src : string to concatenate to dest
- * @n : how many bytes to link from src to dest
- * Return: dest
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
-char *_strncat(char *dest, char *src, int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int l = 0, i;
-while (dest[l])
-{
-l++;
-}
-for (i = 0; src[i] && i < n; i++)
-{
-dest[l] = src[i];
-l++;
-}
-dest[l] = '\0';
-return (dest);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
+
+	return (dest);
 }
