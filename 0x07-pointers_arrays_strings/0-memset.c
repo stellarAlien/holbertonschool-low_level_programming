d1ae6391de44978bdfff3219e4a1ca98d5432f63
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-18 22:10:16 +0100

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,16 @@
-
 /**
 *memset - fill a memory space with a constant
 *@s : string
 *@b : byte 
 *@n : int
+*Return : s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
 int i;
-for (i = 0; i <=  n; i++)
+for (i = 0; i < n; i++)
 {
 s[i] = b;
 }
-return (&(s-i));
+return (s);
 } 
