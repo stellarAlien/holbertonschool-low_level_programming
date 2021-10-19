61e1d23181fd1446893a7bbabcb1d2d5a5c6f683
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-19 19:09:52 +0100

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,13 +5,12 @@
  * @accept: the bytes to include
  *
  * Return: the number of bytes in the initial segment of @s which consist only
- * of bytes from @accept
- */
-
+*of bytes from @accept
+*/
 unsigned int _strspn(char *s, char *accept)
 {
-int i, j;
-int c = 0;
+int i, j, c;
+c = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
 if (s[i] != 32)
