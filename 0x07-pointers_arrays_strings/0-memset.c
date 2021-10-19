bf07bfe1a0af55a1010ec9a6c23f2d80b9b38bfb
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-19 10:33:41 +0100

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,8 @@
-#include "main.h"
-#include <stdio.h>
 /**
- *memset - fill a memory space with a constant
- *@s : string
- * @b: byte 
- * @n: int
+ *_memset - fill a memory space with a constant
+ *@s: stringa
+ *@b: byte 
+ *@n: int
  *Return : s
  */
 char *_memset(char *s, char b, unsigned int n)
