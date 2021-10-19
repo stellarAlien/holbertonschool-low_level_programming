fcddfec556235a15a7420ea24180fea1bcf65ef3
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-19 20:05:07 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,10 +7,13 @@
 * @n: bytes filled.
 * Return: the pointer to dest.
 */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {	
 unsigned int i;
 for (i = 0; i < n; i++)
+{
 *(dest + i) =  *(src + i);
+}
 return (dest);
 }
