ebeaeded3c6d774cc3f83dc9f96e99f6da6e44fe
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-19 15:22:23 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	if (c == '\0')
-		return (&s);
-	/*return ('\0');*/
-
-	while (*s)
-	{
-		if (*s != c)
-		s++;
-		else
-	return (&s);
-	}
-	
+while (*s)
+{
+if (*s != c)
+s++;
+else
+return (s);
+}
+if (c == '\0')
+return (s);
+return ('\0');
 }
