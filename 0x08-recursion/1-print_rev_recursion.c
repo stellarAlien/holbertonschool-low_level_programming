1053ad52e599938243886b8e299f2f47a7afe092
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-20 22:42:14 +0100

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,7 +5,17 @@
  */
 void _print_rev_recursion(char *s)
 {
-if( *(s + 1) == '\0')
+if( *s == '\0')
+{
+_putchar('\0');
+return;
+}
+
+else if(*s == '\n')
+{
+_putchar('\n');
+}
+else if ( (*(s + 1) == '\0'))
 {
 _putchar(*s);
 *s = '\0';
