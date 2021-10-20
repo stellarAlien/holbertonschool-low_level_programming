81771d0921d26d0e237e661d157252be8f9e3f0f
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-20 23:10:18 +0100

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,12 +1,13 @@
 #include"main.h"
 /**
  * _strlen_recursion - strlen with recursion
- * @s : string to determine the length of 
+ * @s : string to determine the length of
+ * Return: returns length of s
  */
 int _strlen_recursion(char *s)
 {
 if (*s)
-return ( 1 + _strlen_recursion(s +1));
+return (1 + _strlen_recursion(s + 1));
 else
 return (0);
 }
