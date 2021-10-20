a78080cbd9218768426acc06311ec2f0a961631b
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-20 22:01:04 +0100

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,12 @@
-#include"main.h"
 /**
  *  _puts_recursion(char *s) -  puts a string recursively
  *  @s: string to print 
  */
-
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
 putchar('\n');
-exit(0);
 }
 putchar(*s);
  _puts_recursion(s++);
