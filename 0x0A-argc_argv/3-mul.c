548cea30c84fa4a6510bd0505e33e4f79b13c7c6
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-22 22:56:45 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,20 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+
 /**
- * main - a*b;
- * @argc: argcount
- * argc: argvector
- * Return: 1 or 0
+ *main - prints all arguments it receives.
+ *@argc: size parameters
+ *@argv: array pointer parameters
+ *Return: 0 success
  */
 int main(int argc, char *argv[])
 {
-int a, b;
-if (argc > 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-else
-{
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-printf("%d\n", a * b);
-return (0);
-}
-}
+	int i = 0;
 
+	if (argc >= 0)
+	{
+		for (i = 0; i < argc; i++)
+			printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
