de8d32e39de22c061b856f85c0c3eb31407e35ae
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-22 11:31:34 +0100

    isdigit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+int i,s, l, j;
+s = 0;
+j = 0;
+l = 0;
+	if(*argc < 1)
+	{
+		print("0\n");
+	}
+	else
+	{
+	for(i = 0; argv[i]; i++)
+	{
+	l = strlen(argv[i]);
+	for ( j = 0; j < l ; j++)
+	{
+	if (isdigit(argv[i]) && argv[i] > 0)
+	{
+	s += argv[i];
+	}
+	else
+	{
+	printf("Error\n");
+	return(1);
+	}
+	}
+	}
+	printf("%u", s);
+	}
+	return(0);
+}
+	
+
