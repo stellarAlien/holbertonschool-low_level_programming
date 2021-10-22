55e8bba1d20e4c8374c8004ee2e95565bb3b2236
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-22 22:20:57 +0100

    2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
+/**
+ * main - print all argvarguments
+ *@argc: argv size
+ * @argv: argv
+ * Return: always 0
+ */
 int main(int argc, char *argv[])
 {
 int i;
-for(i = 0; i <= *argc; i++)
+for(i = 0; i < argc; i++)
 {
 printf("%s", argv[i]);
 return(0);
