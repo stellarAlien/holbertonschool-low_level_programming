86e3a18534ef387ab01f3ec836d2ef810b772c27
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-22 23:02:33 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,42 +1,39 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 /**
- *  *main - adds positive numbers.
- *   *@argc: size parameters
- *    *@argv: array pointer parameters
- *     *Return: 0 success or 1 error
+ *main - adds positive numbers.
+ *@argc: size parameters
+ *@argv: array pointer parameters
+ *Return: 0 success or 1 error
  */
 int main(int argc, char *argv[])
 {
-int i, s, l, j;
-s = 0;
-j = 0;
-l = 0;
-	if(argc < 2)
-	{
-		print("0\n");
-	}
-	else
-	{
-	for(i = 0; argv[i]; i++)
-	{
-	l = strlen(argv[i]);
-	for ( j = 0; j < l ; j++)
-	{
-	if (isdigit(argv[i]) && argv[i] > 0)
-	{
-	s += argv[i];
-	}
-	else
-	{
-	printf("Error\n");
-	return(1);
-	}
-	}
-	}
-	printf("%u", s);
-	}
-	return(0);
-}
+	int i, j;
+	unsigned int add = 0;
 
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j]; j++)
+			{
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			add += atoi(argv[i]);
+		}
+		printf("%d\n", add);
+	}
+
+	return (0);
+}
