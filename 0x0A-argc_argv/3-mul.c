b7356c9403a02145166012f49e67d177df2eeca6
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-22 22:46:07 +0100

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
-int main(void)
+/**
+ * main - a*b;
+ * @argc: argcount
+ * argc: argvector
+ * Return: 1 or 0
+ */
+int main(int argc, char *argv[])
 {
 if ( argc < 3 )
 	{
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
 else
 {
-printf("%d", val(argv[1])*vala(rgv[2]));
-return(0);
+printf("%d", val(argv[1])*val(argv[2]));
+return (0);
 }
 }
+
