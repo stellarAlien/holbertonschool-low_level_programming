660035badb5926ac9ab465b0fc67865cde9a2440
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-22 13:03:43 +0100

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
-int main(__attribute__((unused) int argc , char *argv))
+/**
+ * main - prints program name
+ * @argc - arrgcount 
+ * @argv - argvect
+ */
+int main(__attribute__((unused)) int argc , char *argv[])
 {
-printf("%s", argv[0]);
-return(0);
+printf("%s\n", argv[0]);
+return (0);
 }
 
