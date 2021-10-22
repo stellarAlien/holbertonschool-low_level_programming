44d0407a3d5e4423f0459234283e765ba215f3ac
root <root@LAPTOP-8L71RL06.localdomain>
2021-10-22 22:10:31 +0100

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,10 +4,11 @@
  *main - print argc
  *@argc: argcount
  *@argv: argvector
+ *Return: always 0
  */
 int main(int argc,  __attribute__((unused)) char *argv[])
 {
 printf("%d", argc);
-return(0);
+return (0);
 }
 
