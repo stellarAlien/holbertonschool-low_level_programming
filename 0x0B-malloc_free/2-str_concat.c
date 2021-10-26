00f988d1a354f843ead70e985653326c36cfc1ff
Seif <saifgaida@gmail.com>
2021-10-26 12:39:17 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,33 +10,34 @@ char *str_concat(char *s1, char *s2)
 {
 	int i, j, k;
 	char *s;
-	if ( s1 == NULL)
+	if (s1 == NULL)
 	{
 		i = 0;
-		else
-			while (s1[i] != '\0')
-			{
-				i++;
-			}
-	}
-	if( s2 === NULL)
-	{
-		j = 0;
 	}
 	else
 		while (s1[i] != '\0')
 		{
 			i++;
-		}            
-	s = (char*)malloc(sizeof(char) * (i + j) + 1);                                                                                                                
-	for (k = 0; k < i; k++)
-	{
-		s[k] = s1[k];
-	}    
-	for (k =0; k < j; k++)
-	{
-		s[k + i] = s2[k];
-	}
-	s[i + k] = '\0';
-	return(s);
+		}
+}
+if(s2 === NULL)
+{
+	j = 0;
+}
+else
+while (s2[j] != '\0')
+{
+	j++;
+}          	  
+s = (char*)malloc(sizeof(char) * (i + j) + 1);                                                                                                                
+for (k = 0; k < i; k++)
+{
+	s[k] = s1[k];
+}    
+for (k =0; k < j; k++)
+{
+	s[k + i] = s2[k];
+}
+s[i + k] = '\0';
+return(s);
 }
