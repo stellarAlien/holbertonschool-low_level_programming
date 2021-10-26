4541d460f09c97f0891b94ee49e438884b49bb9e
Seif <saifgaida@gmail.com>
2021-10-26 13:18:13 -0700

    100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,23 +1,27 @@
 #include<stdlib.h>
 #include<stdio.h>
 /**
- *
+ * argstostr - read from argv
+ * @ac: argc
+ * @av: argv
+ *Return: Null else s
  */
 char *argstostr(int ac, char **av)
 {
+int i;
 char *s;
 s = (char *)malloc((sizeof(char) * ac * 2) + 1);
-if ( s == NULL)
+if (s == NULL)
 {
 printf("not enough memory");
 return (NULL);
 }
-for ( i = 0 ; i <= ac; i += 2)
+for (i = 0; i <= ac; i += 2)
 {
-s[i] = av[i];
+s[i] = *(*(av + i));
 s[i + 1] = '\n';
 }
 s[i - 1] = '\0';
-return(s);
+return (s);
 }
 
