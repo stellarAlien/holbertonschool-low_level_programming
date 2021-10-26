0172b34a1671d62396832ea1e8e739b41b9ed8e1
Seif <saifgaida@gmail.com>
2021-10-26 08:19:11 -0700

    2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include<stdlib.h>
+/**
+ * str_concat - concats two strings
+ * @s1:string uno
+ * @s2: another string
+ *Return: Null if malloc fails and s if not
+ */
+char *str_concat(char *s1, char *s2)
+{
+unsigned int i, j, k, t, m;
+char *s;
+if (*s1 == '\0')
+{
+return (s2);
+}
+else if (*s2 == '\0')
+{
+return (s1);
+}
+while (s1[i] != '\0')
+	{
+		i++;
+	}
+while (*s2 != '\0')
+{
+	j++;
+}
+i++;
+s = malloc(sizeof(char) * (i + j));
+if (s == NULL)
+{
+	fprintf(stderr, "not enough memory");
+	return (NULL);
+}
+for (k = 0; k <= i; k++)
+{
+s[k] = s1[i];
+}
+m = i + j;
+t = 0;
+for (k = 0; k <= (i + j); k++)
+{
+s[k] = s2[t];
+t++;
+}
+s[k] = '\0';
+return (s);
+}
