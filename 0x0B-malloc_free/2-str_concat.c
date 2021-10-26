3218af53247938a7045bc14c04306f6a12b65bef
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-26 20:48:45 +0100

    Update 2-str_concat.c
    cleaner code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,43 +1,32 @@
+#include "main.h"
 #include<stdlib.h>
-#include<stdio.h>
 /**
- *  *  * str_concat - concats two strings
- *   *   * @s1:string uno
- *    *    * @s2: another string
- *     *     *Return: Null if malloc fails and s if not
+ * str_concat - entry point
+ * @s1:char
+ * @s2:char
+ * Return:Null or pointer
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, k;
-	char *s;
-	if (s1 == NULL)
-	{
-		i = 0;
-	}
-	else
-		while (s1[i] != '\0')
-		{
-			i++;
-		}
-}
-if(s2 === NULL)
-{
-	j = 0;
-}
+unsigned int i, j, k;
+char *s;
+if (s1 == NULL)
+i = 0;
 else
-while (s2[j] != '\0')
-{
-	j++;
-}          	  
-s = (char*)malloc(sizeof(char) * (i + j) + 1);                                                                                                                
+for (i = 0; s1[i]; i++)
+;
+if (s2 == NULL)
+j = 0;
+else
+for (j = 0; s2[j]; j++)
+;
+s = malloc((i + j + 1) * sizeof(char));
+if (s == 0)
+return (NULL);
 for (k = 0; k < i; k++)
-{
-	s[k] = s1[k];
-}    
-for (k =0; k < j; k++)
-{
-	s[k + i] = s2[k];
-}
-s[i + k] = '\0';
-return(s);
+s[k] = s1[k];
+for (k = 0; k < j; k++)
+s[k + i] = s2[k];
+s[i + j] = '\0';
+return (s);
 }
