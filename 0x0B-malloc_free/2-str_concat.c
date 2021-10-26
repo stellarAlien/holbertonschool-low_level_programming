d54088a2d4b34e31e545223189510cc4d94b5fa4
Seif <saifgaida@gmail.com>
2021-10-26 12:35:18 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,50 +1,42 @@
 #include<stdlib.h>
 #include<stdio.h>
 /**
- * str_concat - concats two strings
- * @s1:string uno
- * @s2: another string
- *Return: Null if malloc fails and s if not
+ *  *  * str_concat - concats two strings
+ *   *   * @s1:string uno
+ *    *    * @s2: another string
+ *     *     *Return: Null if malloc fails and s if not
  */
 char *str_concat(char *s1, char *s2)
 {
-unsigned int i, j, k, t;
-char *s;
-if (*s1 == '\0')
-{
-return (s2);
-}
-else if (*s2 == '\0')
-{
-return (s1);
-}
-while (s1[i] != '\0')
+	int i, j, k;
+	char *s;
+	if ( s1 == NULL)
 	{
-		i++;
+		i = 0;
+		else
+			while (s1[i] != '\0')
+			{
+				i++;
+			}
 	}
-while (s2[j] != '\0')
-{
-	j++;
-}
-i++;
-s = malloc(sizeof(char) * (i + j));
-if (s == NULL)
-{
-	fprintf(stderr, "not enough memory");
-	return (NULL);
-}
-t = 0;
-for (k = 0; k <= i; k++)
-{
-s[k] = s1[t];
-t++;
-}
-t = 0;
-for (k = i; k <= (i + j); k++)
-{
-s[k] = s2[t];
-t++;
-}
-s[k] = '\0';
-return (s);
+	if( s2 === NULL)
+	{
+		j = 0;
+	}
+	else
+		while (s1[i] != '\0')
+		{
+			i++;
+		}            
+	s = (char*)malloc(sizeof(char) * (i + j) + 1);                                                                                                                
+	for (k = 0; k < i; k++)
+	{
+		s[k] = s1[k];
+	}    
+	for (k =0; k < j; k++)
+	{
+		s[k + i] = s2[k];
+	}
+	s[i + k] = '\0';
+	return(s);
 }
