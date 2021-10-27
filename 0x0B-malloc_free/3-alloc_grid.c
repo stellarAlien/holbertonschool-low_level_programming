02151329f1273e8d9b9aaba6e9ade6c13c4a944d
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-10-27 18:21:08 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,10 +16,12 @@ return (NULL);
 }
 arr = malloc(sizeof(int*) * height);
 for (i = 0; i < height; i++)
+{
 arr[i] = malloc(sizeof(int) * width);
 for (j = 0; j < width; j++)
 {
 arr[i][j] = 0;
+}
 }
 return (arr);
 }
