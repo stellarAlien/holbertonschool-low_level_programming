d73f8b6df1c2bf8f37dbf6872eae03f62ef4d1a6
Seif <saifgaida@gmail.com>
2021-10-27 10:14:30 -0700

    3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ *alloc_grid - initializes 2d array with zeo
+ *@width: width of array
+ *@height: height of array
+ *Return: Null if width or height == 0 else arr 
+ */
+int **alloc_grid(int width, int height)
+{
+int i, j;
+int **arr;
+if (width == 0 || height == 0)
+{
+return (NULL);
+}
+*arr = malloc(sizeof(int*) * height);
+for (i = 0; i < height; i++)
+arr[i] = malloc(sizeof(int) * width);
+for (j = 0; j < width; j++)
+{
+arr[i][j] = 0;
+}
+return (arr);
+}
+
