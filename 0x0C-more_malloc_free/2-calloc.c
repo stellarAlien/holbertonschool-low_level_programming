7c7c227a51c64545865f25c543aab30e2e789716
Seif <saifgaida@gmail.com>
2021-10-28 11:48:06 -0700

    2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,42 @@
+#include<stdlib.h>
+/**
+ * calloc - allocate memory contiguously
+ * @nmemb: number of memory blocks
+ * @size: size of each block
+ * Return: Null in failure or arr
+ */
+ void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	int *p, *b;
+
+	if (nmemb == 0 || size ==0)
+	{
+		return (NULL);
+	}
+	P = (int *)malloc(size);
+	b = p;
+	*b = 0;
+	   if(p == NULL)
+	   {
+		return (NULL);
+		free(p);
+		free(b);
+	   }
+	for (i = 0; i < nmemb; i++)
+	{
+		b = p + size * i;
+		b = (int *)malloc(size);
+		if (b == NULL)
+		{
+			for (j = i; j >=0; j--)
+			{
+				free(p + j * size);			
+			}
+			free(p);
+			free(b);
+			return (NULL);
+	}
+	}
+	return(p);
+}
