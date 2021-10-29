0b21f9d96a49301cd60daeeb66ced2c8f6587db4
Seif <saifgaida@gmail.com>
2021-10-29 04:50:00 -0700

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,23 +5,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
-{
-_putchar('\0');
-return;
-}
-
-else if (*s == '\n')
-{
-_putchar('\n');
-_print_rev_recursion(s++);
-}
-else if ((*(s + 1) == '\0'))
-{
+if (*s)
+	{
+		_print_rev_recursion(s + 1);
+	}
 _putchar(*s);
-_print_rev_recursion(--s);
 }
-else
-s = s + 1;
-}
-
