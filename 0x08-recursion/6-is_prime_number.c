157fa5f64024fb38243ee7608fdaaac564674033
Seif <saifgaida@gmail.com>
2021-10-29 05:37:28 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,28 +1,30 @@
 #include "main.h"
 /**
- * is_prime_number - entry point
- * @n:int
- * Return:void
+ * verif - verifies if number is prime
+ * @n: integer to check if prime
+ * Return: 1 if 
+ */
+int verif(int n)
+{
+	float i, ran;
+	ran = n/2;
+
+	for (i = 2; i <= ran; i++)
+	{
+		if ((n % (int)(i)) == 0)
+		{
+			return(0);
+		}
+		return (1);
+	}
+}
+/**
+ *is_prime_number: checks if number is prime
+ *@n: number to check
  */
 int is_prime_number(int n)
 {
-if (n == 2)
-return (1);
-else
-return (prime_checker(2, n));
-}
-/**
- * prime_checker - entry point
- * @n:int
- * @p:int
- * Return:int
- */
-int prime_checker(int p, int n)
-{
-if ((n < 2) || (n % p == 0))
-return (0);
-else if (p > (n / 2))
-return (1);
-else
-return (prime_checker(p + 1, n));
+	int i;
+	i = verif(n);
+	return(i);
 }
