2d89da6241740db721830c4d7be3012e22a5a689
Seif <saifgaida@gmail.com>
2021-11-01 11:14:50 -0700

    2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 /**
- *main - include header 
+ *main - include header
+ *Return: Always 0
  */
-void main (void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
+	return (0);
 }
