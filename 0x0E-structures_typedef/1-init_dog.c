343bc7f8575d9650273c7c7421a41312ea2e354e
Seif <saifgaida@gmail.com>
2021-11-01 09:10:40 -0700

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog - initialize dog struct
+ *@name: dog's name
+ *@d: struct for dog
+ *@age: dog's age
+ *@owner: dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
+
