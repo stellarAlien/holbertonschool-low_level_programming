b54818dac0b55f24ec5a806fd19ae0ec78fc1a26
Seif <saifgaida@gmail.com>
2021-11-02 03:43:43 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,13 +8,13 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
 		if ((*d).name == NULL)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", (*d).name);
-		if ((*d).age == 0.0f)
+		if ((*d).age == 0)
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", (*d).age);
