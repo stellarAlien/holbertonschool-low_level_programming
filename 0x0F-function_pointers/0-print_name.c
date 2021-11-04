0386bde761872876174ee10f1816d8bb2903e88c
Seif <saifgaida@gmail.com>
2021-11-04 04:12:38 -0700

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to a function
+ *Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
