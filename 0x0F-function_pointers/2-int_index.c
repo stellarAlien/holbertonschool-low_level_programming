4c316b484e11eaa4dfad5b1bbaf04cbbb168f036
Seif <saifgaida@gmail.com>
2021-11-04 05:35:04 -0700

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,9 +12,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if  (size <= 0)
 		return (-1);
+	else if (array && cmp)
+	{
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 			return (i);
 	}
+	}
+	else
+		return (-1);
 }
