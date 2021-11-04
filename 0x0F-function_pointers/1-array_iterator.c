32ccaa7c8d7a8623e281fa238e68c1728a7b0692
Seif <saifgaida@gmail.com>
2021-11-04 04:35:20 -0700

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+/**
+ *array_iterator - iterate an action on an array
+ *@array: array to go through
+ *@size: size of array
+ *@action: pointer to action function
+ *Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = action(array[i]);
+	}
+}
