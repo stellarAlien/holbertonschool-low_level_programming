f1ca1b6047e10b0393291da05ee577e1e8b07522
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-11-05 23:21:18 +0100

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,14 +6,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int i;
+unsigned int i, x;
 va_list l;
 double sum = 0;
 if (n == 0)
 {
 return (0);
 }
-va_start(l, n);
+va_start(l, x);
 for (i = 0; i < n; i++)
 {
 sum += va_arg(l, int);
