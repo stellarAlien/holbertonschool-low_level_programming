3e0cdead52c2d6713586d83e83b4a8d8a7d335f1
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-11-10 16:24:59 +0100

    Update 100-hello_world.asm

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -4,7 +4,7 @@
           section   .text
 main:                                       ; This is called by the C library startup code
           mov       rdi, message            ; First integer (or pointer) argument in rdi
-          call      puts                    ; puts(message)
+          syscall
           ret                               ; Return from main back into C library wrapper
 message:
           db        "Hello, World", 0        ; Note strings must be terminated with 0 in C
