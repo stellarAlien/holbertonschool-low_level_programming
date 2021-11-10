942e4fa3bf3267cd435afe9015e17b35c983f9dc
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-11-10 15:24:47 +0100

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,32 +1,31 @@
-#include<stdarg.h>
-#include<stdio.h>
-#include<stdlib.h>
-void print_numbers(const char *separator, const unsigned int n, ...)
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <unistd.h>
+/**
+ * print_numbers - prints out numbers from variadic function
+ * @seperator: pointer to seperation string
+ * @n: number of numbers to print
+ * @...: numbers to print
+ *
+ * Return: always void
+ */
+void print_numbers(const char *seperator, const unsigned int n, ...)
 {
-size_t i;
-va_list(l);
+	va_list arg;
+	unsigned int i;
+	int num;
 
-	if (n == 0)
-		return;
-	va_start(l, n);
+	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
-		if (separator == NULL)
-		{
-			printf("%d ", va_arg(l, int));
-		}
-			else
-			{
-				if (i == n - 1)
-				printf("%d", va_arg(l, int));
-				else
-				{
-					printf("%d%s", va_arg(l, int), separator);
-				}
-
-			}
+		num = va_arg(arg, int);
+		printf("%d", num);
+		if (seperator == NULL || *seperator == '\0')
+			continue;
+		if (i != n - 1)
+			printf("%s", seperator);
 	}
-	printf("\n");
-	va_end(l);
+	putchar('\n');
+	va_end(arg);
 }
-
