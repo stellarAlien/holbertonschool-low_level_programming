ae765d0cc8cb4221ca45fd7ef2859480d9f47e42
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-11-10 16:52:46 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,11 @@
 #include<stdarg.h>
 #include<stddef.h>
 #include<stdio.h>
+typedef struct print_convert_variable
+{
+	char type;
+	void (*f)(va_list *);
+} conv_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
