5688bde0b0970641fab4843afa3cf2030a33ed29
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-11-10 16:20:15 +0100

    Update 100-hello_world.asm
    for gcc

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,15 +1,10 @@
-
-          global    _start
+          global    main
+          extern    puts
 
           section   .text
-_start:   mov       rax, 1                  ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 13                 ; number of bytes
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 60                 ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
-
-          section   .data
-message:  db        "Hello, World", 10      ; note the newline at the end
+main:                                       ; This is called by the C library startup code
+          mov       rdi, message            ; First integer (or pointer) argument in rdi
+          call      puts                    ; puts(message)
+          ret                               ; Return from main back into C library wrapper
+message:
+          db        "Hello, World", 0        ; Note strings must be terminated with 0 in C
