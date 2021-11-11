aa20fced284d07d84f020183f2bd75d375b7b181
Seif <saifgaida@gmail.com>
2021-11-11 14:42:22 -0800

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,12 +3,12 @@
 void construct(void) __attribute__((constructor));
 
 /**
- *Construct- Prints a string before the
+ *construct-  Prints a string before the
  *main function is executed.
  */
 
 void construct(void)
 {
-	    printf("You're beat! and yet, you must allow,\n");
-	        printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
