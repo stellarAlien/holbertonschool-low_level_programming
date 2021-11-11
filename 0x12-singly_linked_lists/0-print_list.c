a42d81d375bf0578681e0073c4bb5bb6d8c74e01
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-11-12 00:00:46 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,16 @@
 
 size_t print_list(const list_t *h)
 {
-size_t i = 0;
+  size_t i = 0;
 
-while (h != NULL)
-{
-if (h->str != NULL)
-printf("[%d] %s\n", h->len, h->str);
-else if (h->str == NULL)
-printf("[0] (nil)\n");
-i++;
-h = h->next;
-}
-return (i);
+  while (h != NULL)
+  {
+    if (h->str != NULL)
+      printf("[%d] %s\n", h->len, h->str);
+    else if (h->str == NULL)
+       printf("[0] (nil)\n");
+    i++;
+    h = h->next;
+  }
+  return (i);
 }
