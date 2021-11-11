779e96cb094a0c31835ef82bd0774be482934808
Seif <saifgaida@gmail.com>
2021-11-11 14:30:00 -0800

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void construct(void) attribute ((constructor));
+
+/**
+ *  * Func - Prints a string before the
+ *        main function is executed.
+ */
+
+void construct(void)
+{
+	    printf("You're beat! and yet, you must allow,\n");
+	        printf("I bore my house upon my back!\n");
+}
