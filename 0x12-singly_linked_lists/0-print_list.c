714f2595238942b19532d06797bf65ecf7677541
Seif <saifgaida@gmail.com>
2021-11-11 10:25:46 -0800

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,39 +1,22 @@
+#include <stdio.h>
 #include "lists.h"
-#include<stdio.h>
-#include<stdlib.h>
+
 /**
- *print_list - prints a linked list
- *@h: linked list
- *Return: 1 if  success else 0
+ *  * print_list - prints all the elements of a list_t list
+ *   * @h: Listed lists
+ *    * Return: counter
  */
 size_t print_list(const list_t *h)
 {
-size_t i, j;
-const list_t *p = h; /*(list_t *)malloc(sizeof(list_t));*/
-char *s;
+unsigned int count = 0;
 
-	i = 0;
-	p = h;
-	/*s = h->str;*/
-	while(p->next != NULL)
-	{
-	if (p->str == NULL)
-	{
-	printf("[0] (nil)");
-	}
-	else
-	{
-	j = 0;
-	s = p->str;
-	while(s[j] != '\0')
-	{
-	printf("%s", s);
-	j++;
-	}
-	}
-	p = p->next;
-	i++;
-	}
-	return (i);
+while (h)
+{
+	count++;
+	h->str ? printf("[%d] %s\n", h->len, h->str) :
+		printf("[0] (nil)\n");
+	h = h->next;
+}
+return (count);
 }
 
