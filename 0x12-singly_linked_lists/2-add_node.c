c8494ad3672da942803759df4da62128d4b44c00
Seif <saifgaida@gmail.com>
2021-11-12 09:34:50 -0800

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,22 @@
 #include "lists.h"
 
 /**
+ * str_cnt - counts string length;
+ *@str: string to check
+ *Return: string length
+ */
+int str_cnt(char *str)
+{
+	int i;
+	i = 0;
+	while(str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+	
+/**
  *add_node - adds node to list
  *@head: head of node list
  *@str: string to insert
@@ -11,12 +27,21 @@ list_t *add_node(list_t **head, const char *str)
 {
 list_t *p;
 
-	(*p)->next = h;
-	(*p)->str = str;
-	return (p);
+if (str == NULL)
+	return (NULL);
+if (*head == NULL)
+	return (NULL);
+p = malloc(sizeof(list_t);
+if (p == NULL)
+return (NULL);
+		p->str = strdup(str);
+		 if (p->str == NULL)i
+		 {
+		 free(p);
+		 return (NULL);
+		 }
+		 p->next = *h;
+		p->len = str_cnt(str);
+		*head = p;	
+		return (p);
 }
-
-
-
-
-
