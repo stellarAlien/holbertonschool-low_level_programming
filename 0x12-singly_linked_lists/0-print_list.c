c2391589f5b2ed2d961f7df704b0aa55b8466a9c
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-11-12 11:40:15 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,25 +2,23 @@
 #include <stdio.h>
 #include <string.h>
 #include "lists.h"
-
 /**
- * print_list - print the element of the list
+ * print_list - print the elements of the list and returns list count 
  * @h: head of the list
- * Return: the element of the list
+ * Return: number of items
 */
-
 size_t print_list(const list_t *h)
 {
-size_t i = 0;
+        size_t i = 0;
 
-while (h != NULL)
-{
-	if (h->str != NULL)
-		printf("[%d] %s\n", h->len, h->str);
-	else if (h->str == NULL)
-		printf("[0] (nil)\n");
-	i++;
-	h = h->next;
-}
-return (i);
+        while (h != NULL)
+        {
+                if (h->str != NULL)
+                        printf("[%d] %s\n", h->len, h->str);
+                else if (h->str == NULL)
+                        printf("[0] (nil)\n");
+                i++;
+                h = h->next;
+        }
+        return (i);
 }
