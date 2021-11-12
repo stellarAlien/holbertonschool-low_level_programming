26b6e7afb5a129ae01cf17cdc09b123aca0694a6
Seif <saifgaida@gmail.com>
2021-11-12 03:44:31 -0800

    2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *add_node - adds node to list
+ *@head: head of node list
+ *@str: string to insert
+ *Return: adress of new elemnt or NULL
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *p;
+
+	(*p)->next = h;
+	(*p)->str = str;
+	return (p);
+}
+
+
+
+
+
