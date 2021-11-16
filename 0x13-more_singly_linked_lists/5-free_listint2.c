e2c2fa2d0ef69c665707af5f14dbeb83d79ae6b2
root <root@LAPTOP-8L71RL06.localdomain>
2021-11-16 22:17:18 +0100

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 #include<stdlib.h>
 /**
- *  * free_listint - free a list
+ *  * free_listint2 - free a list
  *   * @head: head of list
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *p;
 
-	
-	
 	while (*head)
 	{
-		p = (*head)->next;	
+		p = (*head)->next;
 		free(*head);
 		*head = p;
 	}
+	free(p);
 }
