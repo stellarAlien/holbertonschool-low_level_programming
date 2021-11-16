86302171e94b347be968dfff905cb03cae981410
root <root@LAPTOP-8L71RL06.localdomain>
2021-11-16 23:24:40 +0100

    9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include<stdlib.h>
+/**
+ * insert_nodeint_at_index
+ * @head: head of list
+ * @idx: index of node
+ * @n: int
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *p, *nw;
+unsigned int i;
+i = 0;
+if (*head == NULL)
+{
+	return (NULL);
+}
+nw = (listint_t *)malloc(sizeof(listint_t));
+if (nw == NULL)
+{
+	return (NULL);
+	printf("ERROR");
+}
+while (i < idx && p)
+{
+p = p->next;
+i++;
+}
+if (p == NULL)
+{
+return (NULL);
+free(nw);
+}
+else
+{
+nw->next = p->next;
+nw->n = n;
+p->next = nw;
+}
+}
