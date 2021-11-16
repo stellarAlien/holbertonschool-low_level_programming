2c92a75270e94922aaf27d3a48d3f156ceef8677
root <root@LAPTOP-8L71RL06.localdomain>
2021-11-16 12:49:24 +0100

    0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - prints a list
+ *@h: head of list
+ *Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t i;
+const list_t *p = h;
+
+i = 0;
+while (p != NULL)
+{
+	printf("%d\n", p.n);
+	i++;
+	p = p.next;
+}
+return (i);
+}
+
