efe7ebd1d56cfbbed51978bf75ef6191a8673a5c
root <root@LAPTOP-8L71RL06.localdomain>
2021-11-16 13:32:29 +0100

    3

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include<stdlib.h>
+/**
+ * add_nodeint_end - add note at the end
+ * @head: head of list
+ * @n: content of node
+ * Return: head of list or null if process failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *p;
+
+if (*head == NULL)
+{
+return (NULL);
+}
+p = *head;
+do
+{
+p = p->next;
+}
+while(p->next != NULL);
+p->next =(listint_t *)malloc(sizeof(listint_t));
+if (p->next == NULL)
+return (NULL);
+p->next->n = n;
+return (p->next);
+}
+
+
+
+
