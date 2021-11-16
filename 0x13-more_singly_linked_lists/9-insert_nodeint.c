7bcee802c560e9c037122b1ffb02273aafc4a1b8
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-11-17 00:00:00 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,42 +1,65 @@
+#include <stdlib.h>
 #include "lists.h"
-#include<stdlib.h>
+
 /**
- * insert_nodeint_at_index - inserts a node at index
- * @head: head of list
- * @idx: index of node
- * @n: int
- *Return:adress of new node or NULL
+ * _listint_len - returns the number of elements in a linked listint_t list.
+ * @h: linked list type listint_t
+ * Return: the number of nodes
+ */
+size_t _listint_len(const listint_t *h)
+{
+	unsigned int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: linked lists
+ * @idx: index to linked list
+ * @n: number to linked list[index]
+ * Return: node by index
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *p, *nw;
-unsigned int i;
+	unsigned int i = 0;
+	listint_t *node = *head, *newNode;
 
-i = 0;
-if (*head == NULL)
-{
+	if (!head || idx > _listint_len(*head))
+		return (NULL);
+
+	if (!idx)
+	{
+		newNode = malloc(sizeof(listint_t));
+		if (!newNode)
+			return (NULL);
+		newNode->next = *head;
+		newNode->n = n;
+		*head = newNode;
+		return (newNode);
+	}
+
+	while (node)
+	{
+		if (i == (idx - 1))
+		{
+			newNode = malloc(sizeof(listint_t));
+			if (!newNode)
+				return (NULL);
+
+			newNode->n = n;
+			newNode->next = node->next;
+			node->next = newNode;
+			return (newNode);
+		}
+		node = node->next;
+		i++;
+	}
 	return (NULL);
-}
-nw = (listint_t *)malloc(sizeof(listint_t));
-if (nw == NULL)
-{
-	return (NULL);
-	printf("ERROR");
-	free(nw);
-}
-p = *head;
-while (p != NULL)
-{
-if (i == idx)
-{
-	nw->next = p->next;
-	nw->n = n;
-	p->next = nw;
-	return (nw);
-}
-p = p->next;
-i++;
-}
-free(nw);
-return (NULL);
 }
