dfff62c76f1e209b0ddebbfbc26e49c99e41d5dd
root <root@LAPTOP-8L71RL06.localdomain>
2021-11-18 18:05:56 +0100

    2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_bit - determines bit state at certain position
+ * @n: int to check
+ * @index: position of index
+ * Return: state of bit or NULL
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i;
+
+	i = (n >> index) & 1;
+	return (i);
+}
