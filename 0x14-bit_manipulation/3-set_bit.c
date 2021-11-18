f53630a6477ab58c60566c8b0d81618b98618f9b
root <root@LAPTOP-8L71RL06.localdomain>
2021-11-18 20:50:26 +0100

    3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * set_bit - sets bit to 1
+ * @n: unsigned int
+ * @index: index of bit
+ * Return: 1 if success else -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index > 32)
+	return (-1);
+if (*n >> index)
+return (1);
+else
+*n |= 1 << index;
+return (1);
+}
+
