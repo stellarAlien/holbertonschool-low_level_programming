5c78440baf562ff75175059253fe3eec519a20b5
root <root@LAPTOP-8L71RL06.localdomain>
2021-11-19 16:49:29 +0100

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,70 +1,21 @@
-#include "dog.h"
-#include <stdlib.h>
-char *cpy(char *dest, char *source);
-int length(char *name);
+#ifndef DOG_H
+#define DOG_H
 /**
- * new_dog - Short description
- * @name:char
- * @age:float
- * @owner:char
- * Return:void
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
  */
-dog_t *new_dog(char *name, float age, char *owner)
+typedef struct dog
 {
-dog_t *nd;
-nd = malloc(sizeof(dog_t));
-if (nd == NULL)
-return (NULL);
-if (name == NULL)
-nd->name = NULL;
-else
-{
-nd->name = malloc(sizeof(length(name) + 1));
-if (nd->name == NULL)
-{
-free(nd);
-return (NULL);
-}
-nd->name = cpy(nd->name, name);
-}
-if (owner == NULL)
-nd->owner = NULL;
-else
-{
-nd->owner = malloc(sizeof(length(owner) + 1));
-if (nd->owner == NULL)
-{
-free(nd);
-return (NULL);
-}
-nd->owner = cpy(nd->owner, owner);
-}
-nd->age = age;
-return (nd);
-}
-/**
- * length - Short description
- * @name:char
- * Return:int
- */
-int length(char *name)
-{
-int i = 0;
-for (; name[i] != '\0'; i++)
-;
-return (i);
-}
-/**
- * cpy - Short description
- * @dest:char
- * @source:char
- * Return:char
- */
-char *cpy(char *dest, char *source)
-{
-int i = 0;
-while (dest[i++])
-dest[i] = source[j];
-dest[i] = '\0';
-return (dest);
-}
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
