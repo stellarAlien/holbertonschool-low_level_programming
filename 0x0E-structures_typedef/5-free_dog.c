db560f8d228068b7f4122aed767e976661566a6f
root <root@LAPTOP-8L71RL06.localdomain>
2021-11-19 16:53:58 +0100

    5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include<stdlib.h>
 /**
  * free_dog
  * @d: pointer to dog's info
@@ -7,7 +8,7 @@
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
-	{	
+	{
 	free(d->name);
 	free(d->owner);
 	free(d);
