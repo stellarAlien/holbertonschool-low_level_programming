e18a2991d9d0ed29ac9b97ff903e20a1a912c7b5
root <root@LAPTOP-8L71RL06.localdomain>
2021-11-22 23:07:25 +0100

    2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * append_text_to_file - appends text to a file
+ * @filename: name of file
+ * @text_content: content of text
+ * Return: 1 if success else -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd, len, w;
+if (filename == NULL)
+	return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
+	return (-1);
+if (text_content != NULL)
+{
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+	w = write(fd, text_content, len);
+	if (w == -1)
+		return (-1);
+}
+close(fd);
+return (1);
+}
