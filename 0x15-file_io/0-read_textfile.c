b3f02e7998d5c110a757d63e309c2828ca862b2a
root <root@LAPTOP-8L71RL06.localdomain>
2021-11-22 20:54:19 +0100

    0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * read_textfile - Read text from a file and print it
+ * @filename:string
+ * @letters:size_t
+ * Return:Read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd, rd, wr;
+char *s;
+char *s = malloc(sizeof(char) * letters);
+
+if (filename == NULL)
+	return (0);
+fd = open(filename, O_RDWR);
+if (fd == -1)
+	return (0);
+rd = read(fd, s, letters);
+if (rd == -1)
+	return (0);
+wd = write(STDOUT_FILENO, s, rd);
+if (wr == -1 || wr < (int)letters)
+	return (0);
+close(fd);
+free(s);
+return (wr);
+}
+
