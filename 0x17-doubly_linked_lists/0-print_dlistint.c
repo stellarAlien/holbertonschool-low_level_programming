cd76e8369f56cfddb9b7af3eadbd5abf300e69c0
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-06 16:56:50 +0100

    0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *print_dlistint - prints all the elements of a double linked list
+ *@h: dlistint_t
+ *Return: list len
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+	
+
+	for (i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
