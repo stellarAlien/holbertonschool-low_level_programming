b63df147932b7a56150538b36cfd70c52e954327
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-06 21:15:32 +0100

    4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *free_dlistint - frees a doubly linked list
+ *@head: head of list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (!head)
+	return (NULL);
+	while (head && head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
+}
