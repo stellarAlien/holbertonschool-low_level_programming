5f0b1c44dc52711f0a7134ee85cf605bc94b8f19
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-28 23:52:24 +0100

    2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,14 +1,14 @@
 #include "hash_tables.h"
 /**
- *key_index: calculates the index of the key passed
+ *key_index - calculates the index of the key passed
  *@key: key passed
- *size: size of array of hashtable
+ *@size: size of array of hashtable
+ *Return: index of hash table
   */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int ind;
 
-	if(key == NULL)
+	if (key == NULL)
 	{
 		return (0);
 	}
