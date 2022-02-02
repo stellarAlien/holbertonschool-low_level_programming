ce668abc37c37d31a09473c492d4d6e9ffa6c6f0
root <root@LAPTOP-8L71RL06.localdomain>
2022-02-02 23:34:03 +0100

    8

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,6 +22,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	idx = key_index((unsigned char *)key, ht->size);
+	if (idx > ht->size)
+		return (0);
 	if (ht->array[idx] != NULL)
 	{
 		temp = ht->array[idx];
