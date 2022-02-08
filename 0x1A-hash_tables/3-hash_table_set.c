e2b281fc23d256b02256d01f9a20613f928991c7
root <root@LAPTOP-8L71RL06.localdomain>
2022-02-09 00:40:13 +0100

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,6 +3,19 @@
 #include<stdlib.h>
 #include<string.h>
 /**
+ *update_val - updates key value in case value is existant
+ *@temp: temporary node to look for insertion position
+ *@new_node: node to hold key value pair
+ */
+void update_val(hash_node_t *temp, hash_node_t *new_node)
+{
+		free(temp->value);
+		temp->value = strdup(new_node->value);
+		free(new_node->value);
+		free(new_node->key);
+		free(new_node);
+}
+/**
  *hash_table_set - adds element to hash table
  *@ht: hash table
  *@key: key of the element
@@ -11,10 +24,12 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	hash_node_t *new_node, *temp;
 	unsigned long int idx;
-	hash_node_t *temp;
 
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 	if (new_node == NULL)
@@ -22,8 +37,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	idx = key_index((unsigned char *)key, ht->size);
-	if (idx > ht->size)
-		return (0);
 	if (ht->array[idx] != NULL)
 	{
 		temp = ht->array[idx];
@@ -39,14 +52,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			ht->array[idx] = new_node;
 		}
 		else
-		{
-			free(temp->value);
-			temp->value = strdup(new_node->value);
-			free(new_node->value);
-			free(new_node->key);
-			free(new_node);
-		}
-	}
+			update_val(temp, new_node);
 	else
 	{
 		new_node->next = NULL;
