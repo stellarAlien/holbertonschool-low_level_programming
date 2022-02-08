67c1a1d36d504129f510efe39d488a049596ad99
root <root@LAPTOP-8L71RL06.localdomain>
2022-02-08 23:44:49 +0100

    malloc handle

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,19 +10,21 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i = 0;
 
 	ht = malloc(sizeof(hash_table_t));
-	if(size == 0)
+	if (ht == NULL)
+		return (NULL);
+	if (size == 0)
 		return (NULL);
 	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if(ht->array == NULL)
+	if (ht->array == NULL)
 	{
 		free(ht);
-		return(NULL);
+		return (NULL);
 	}
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		ht->array[i] = NULL;
 	}
 	ht->size = size;
-	return(ht);
+	return (ht);
 }
 
