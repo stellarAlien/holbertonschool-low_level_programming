64b9e23888e00423666fb78122d2524ad4b8e833
root <root@LAPTOP-8L71RL06.localdomain>
2022-02-21 23:25:39 +0100

    4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ *hash_table_get - returns value associated with key
+ *@ht: hash table
+ *@key: key that i am looking for
+ *Return: value associated with key or null
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	hash_node_t *node;
+
+	if (!ht || !key || *key == '\0')
+		return (NULL);
+	i = key_index((unsigned char *)key, ht->size);
+	if (i > ht->size)
+		return (NULL);
+	node = ht->array[i];
+	while (strcmp(node->key, key) != 0)
+	{
+		if (node->next == NULL)
+			return (NULL);
+		node = node->next;
+	}
+	if (node)
+		return (node->value);
+	return (NULL);
+}
