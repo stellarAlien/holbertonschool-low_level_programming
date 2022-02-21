05420e5b4e5d0b037dcc2a9767b7948521a4bcdd
root <root@LAPTOP-8L71RL06.localdomain>
2022-02-22 00:04:58 +0100

    5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include"hash_tables.h"
+/**
+ *hash_table_print -  prints a hash table
+ *@ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i;
+	hash_node_t *node;
+
+	if(!ht || ht->size == 0)
+	{
+		return;
+	}
+	printf("{");
+	for(i = 0; ht->array[i]; i++)
+	{
+		node = ht->array[i];
+		while(node)
+		{
+			printf("\'%s\' : \'%s\'", node->key, node->value);
+			node = node->next;
+		}
+	}
+	printf("}");
+}
+
