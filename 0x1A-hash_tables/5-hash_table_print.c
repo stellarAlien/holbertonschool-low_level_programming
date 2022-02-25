cfd72950743860546b096c77cb57e9fe533b4a77
stellarAlien <saifgaida@gmail.com>
2022-02-26 00:17:36 +0100

    5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,18 +7,21 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned int i;
 	hash_node_t *node;
+	char *sep;
 
 	if (!ht || ht->size == 0)
 	{
 		return;
 	}
+	sep = "";
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
 		while (node)
 		{
-			printf("\'%s\' : \'%s\'", node->key, node->value);
+			printf("%s'%s\' : \'%s\'", sep, node->key, node->value);
+			sep = ", ";
 			node = node->next;
 		}
 	}
