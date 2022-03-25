c97c2c8af39cb38d932c132171818709d9b17d0e
stellarAlien <saifgaida@gmail.com>
2022-03-25 14:15:13 +0100

    header

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,10 +9,8 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *node;
 	char *sep;
 
-	if (!ht || ht->size == 0)
-	{
+	if (ht == NULL || ht->size == 0)
 		return;
-	}
 	sep = "";
 	printf("{");
 	for (i = 0; i < ht->size; i++)
--- 0x1A-hash_tables/5-main.c
+++ 0x1A-hash_tables/5-main.c
@@ -21,6 +21,6 @@ int main(void)
 				        hash_table_set(ht, "Asterix", "Obelix");
 					    hash_table_set(ht, "Betty", "Cool");
 					        hash_table_set(ht, "98", "Battery Street");
-						    hash_table_print(ht);
+						    hash_table_print(NULL);
 						        return (EXIT_SUCCESS);
 }
